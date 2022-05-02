e85335c3e57460f11269760004699a7de1597b13
Ayush Jain <ayushganna67@gmail.com>
2022-05-02 13:59:04 +0530

    [UPDATED] Changed cron schedule, added more jokes

--- update.py
+++ update.py
@@ -1,14 +1,16 @@
 import random
 
-readme_file = open("README.md","r+")
-jokes_file = open("Jokes.txt","r")
+readme_file = open("README.md","r+",encoding = "utf-8")
+jokes_file = open("Jokes.txt","r",encoding = "utf-8")
 
 readme_lines = readme_file.readlines()
 jokes = jokes_file.readlines()
+print(len(jokes))
 
-
+jokes = jokes[2:]
+print(len(jokes))
 a = random.randint(0,len(jokes))
-while (a-1)%3 != 0:  #every new joke starts at line number 3n - 1
+while (a)%3 != 0:  #every new joke starts at line number 3n
     a = random.randint(0,len(jokes))
 
 line = readme_lines.index("Here's a Joke for you -\n")
@@ -17,7 +19,7 @@ readme_lines[line+1] = jokes[a-1]
 readme_lines[line+2] = jokes[a]
 
 readme_file.close()
-readme_file = open("README.md","w+")
+readme_file = open("README.md","w+",encoding = "utf-8")
 readme_file.writelines(readme_lines)
 
 readme_file.close()
