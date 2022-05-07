6357b0f324956047c6cd396e2af0e6082755a2f2
Ayush Jain <ayushganna67@gmail.com>
2022-05-07 21:45:43 +0530

    [UPDATED] Removed print statements and added comments.

--- update.py
+++ update.py
@@ -1,26 +1,33 @@
 import random
 
+#Open both the README and Jokes files.
 readme_file = open("README.md","r+",encoding = "utf-8")
 jokes_file = open("Jokes.txt","r",encoding = "utf-8")
 
-readme_lines = readme_file.readlines()
+#Read contents of these files and store them in a list.
+readme_lines = readme_file.readlines() 
 jokes = jokes_file.readlines()
-print(len(jokes))
 
+#First two lines of jokes.txt can be skipped as it contains attributions to the jokes sources.
 jokes = jokes[2:]
-print(len(jokes))
+
+#Generate a random interger between 0 and len(jokes) and check if it is the starting line of a new joke.
 a = random.randint(0,len(jokes))
 while (a)%3 != 0:  #every new joke starts at line number 3n
     a = random.randint(0,len(jokes))
 
+#Find Index of the line where the edit is to be made.   
 line = readme_lines.index("Here's a Joke for you -\n")
 
+#Replace the old joke line with the new joke line.
 readme_lines[line+1] = jokes[a-1]
 readme_lines[line+2] = jokes[a]
 
 readme_file.close()
+#Reopen README file in write mode and write the new content. 
 readme_file = open("README.md","w+",encoding = "utf-8")
 readme_file.writelines(readme_lines)
 
+#close both the files.
 readme_file.close()
 jokes_file.close()
